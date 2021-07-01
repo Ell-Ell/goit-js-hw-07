5e0992205ccdec48fad02e44f7d55484a70bc05d
Ell-Ell <AllaAkhtyrskaya@gmail.com>
2021-07-01 16:41:20 +0300

    second part of task js01
    вторая часть

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,7 @@
-const liItems = document.querySelectorAll('.item').length;
-console.log(`В списке ${liItems} категории.`);
+const categoriesItem = document.querySelectorAll('.item');
+console.log('В списке', categoriesItem.length, 'категории.');
+
+for (let elem of categoriesItem) {
+  console.log('Категория:', elem.firstElementChild.textContent);
+  console.log('Количество элементов:', elem.lastElementChild.children.length);
+}
