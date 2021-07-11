a8382156e706206565e145752e798aa4b64e8329
Ell-Ell <AllaAkhtyrskaya@gmail.com>
2021-07-11 14:06:31 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -22,13 +22,56 @@ function createBoxes(amount) {
   boxes.appendChild(fragment);
 }
 function destroyBoxes() {
-  boxes.innerHTML = ''; 
+  boxes.innerHTML = '';
 }
 function random() {
-  return Math.random() * 256; 
+  return Math.random() * 256;
 }
 function plusAmount() {
   getAmount();
 }
 render.addEventListener('click', getAmount);
 destroy.addEventListener('click', destroyBoxes);
+
+// function createRandom() {
+//   return `rgb(${(Math.random() * 255) << 0}, ${(Math.random() * 255) << 0}, ${
+//     (Math.random() * 255) << 0
+//   })`;
+// }
+// const refs = {
+//   input: document.querySelector('input'),
+//   renderBtn: document.querySelector('[data-action="render"]'),
+//   destroyBtn: document.querySelector('[data-action="destroy"]'),
+//   boxes: document.querySelector('#boxes'),
+// };
+
+// refs.renderBtn.addEventListener('click', onRender);
+// refs.destroyBtn.addEventListener('click', onDestroy);
+
+// function createRandom() {
+//   return `rgb(${(Math.random() * 255) << 0}, ${(Math.random() * 255) << 0}, ${
+//     (Math.random() * 255) << 0
+//   })`;
+// }
+// let size = 30;
+
+// function onRender(e) {
+//   const { value } = refs.input;
+//   const wrapper = [];
+//   for (let i = 0; i < +value; i += 1) {
+//     const div = document.createElement('div');
+//     div.style.width = `${size}px`;
+//     div.style.height = `${size}px`;
+//     div.style.backgroundColor = createRandom();
+//     size += 10;
+//     wrapper.push(div);
+//   }
+//   //   refs.boxes.innerHTML = '';
+//   refs.boxes.append(...wrapper);
+// }
+
+// function onDestroy() {
+//   size = 30;
+//   refs.input.value = '';
+//   refs.boxes.innerHTML = '';
+// }
